69df7b4f4f19cf03548fef6a7ec4f58befaf5244
benmwhite <bw2121@gmail.com>
2014-09-17 12:43:58 -0500

    minor edit

--- server.R
+++ server.R
@@ -1,19 +1,11 @@
 library(shiny)
 library(ggplot2)
-shinyServer(function(input, output) {
-  
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
+shinyServer(function(input, output) {  
   output$distPlot <- renderPlot({
     x    <- 1:input$n
     probs <- dbinom(x, size = input$n, prob = input$p)
     qplot(x, probs, geom = c("point", "line"), 
           main = paste("Binomial Distribution Density, n = ", input$n),
-          ylab = "p(x)") + theme_bw()
+          ylab = "p(x)", ylim = c(0, 0.5)) + theme_bw()
   })
 })
