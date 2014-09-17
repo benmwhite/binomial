dc07eee045eac3337262997572b2adb41c685886
benmwhite <bw2121@gmail.com>
2014-09-17 16:56:47 -0500

    final edits

--- server.R
+++ server.R
@@ -5,7 +5,7 @@ shinyServer(function(input, output) {
     x    <- 1:input$n
     probs <- dbinom(x, size = input$n, prob = input$p)
     qplot(x, probs, geom = c("point", "line"), 
-          main = paste("Binomial Distribution Density, n = ", input$n),
+          main = paste("Binomial Distribution Density, n = ", as.character(input$n)),
           ylab = "p(x)", ylim = c(0, 0.5)) + theme_bw()
   })
 })
