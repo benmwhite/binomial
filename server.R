0d29d0bd0ff58d62b6a416f44a655dd8cbc4f070
benmwhite <bw2121@gmail.com>
2014-09-18 14:31:31 -0500

    minor graphical changes

--- server.R
+++ server.R
@@ -2,10 +2,10 @@ library(shiny)
 library(ggplot2)
 shinyServer(function(input, output) {  
   output$distPlot <- renderPlot({
-    x    <- 1:input$n
+    x    <- 0:input$n
     probs <- dbinom(x, size = input$n, prob = input$p)
     qplot(x, probs, geom = c("point", "line"), 
           main = paste("Binomial Distribution Density, n = ", as.character(input$n)),
-          ylab = "p(x)", ylim = c(0, 0.5)) + theme_bw()
+          ylab = "p(x)", ylim = c(0, 1)) + theme_bw()
   })
 })
